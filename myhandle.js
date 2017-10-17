fa2e57975dc1197f94329c94d8847fbd3f2dc9eb
niexiaoguang <nxg@lohosoft.com>
2017-10-17 12:37:07 +0800

    update for ignore config from github

--- myhandle.js
+++ myhandle.js
@@ -1,9 +1,57 @@
+const config = {
+	token: "IK2l2L4o3uTpt58LAxsM7EgAntGrR9YW",
+	appid: "wx49646136f945d3d4",
+	encodingAESKey: "J1x0YwCotXSF2sI0tQL9Z9AGNXIzgfeYRdmo9WC8a2J",
+	appsecret: "ccb0f0a4d2e8a0d3b82cdc1b8c4fe0ce"
+};
+
+const url = require("url");
+
+const getTokenUrl =
+	"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" +
+	config.appid +
+	"&secret=" +
+	config.appsecret;
+const fetch = require("node-fetch");
+
 const store = {};
 store.oldImgMediaId = "";
 store.newImgMediaId = "";
 store.oldVoiceMediaId = "";
 store.newVoiceMediaId = "";
+store.token = "";
+getToken(setToken);
+function getToken(callback) {
+	fetch(getTokenUrl)
+		.then(function(res) {
+			return res.json();
+		})
+		.then(function(json) {
+			// console.log(json);
+			let token = json["access_token"];
+			callback(token);
+		});
+}
 
+function setToken(token) {
+	store.token = token;
+	console.log("set token : ", store.token);
+}
+
+function handleVideo(message, myres) {
+	let mediaId = message.MediaId;
+	let getVideoUrl =
+		"https://api.weixin.qq.com/cgi-bin/media/get?access_token=" +
+		store.token +
+		"&media_id=" +
+		mediaId;
+
+	fetch(getVideoUrl).then(function(res) {
+		console.log("get video data :", res.url);
+		let videUrl = res.url;
+		myres.reply(getVideoUrl);
+	});
+}
 function handle(message, req, res) {
 	console.log("message : ", message);
 	type = message.MsgType;
@@ -32,11 +80,14 @@ function handle(message, req, res) {
 			// MsgId: '6477676419349099087'
 			// let mediaIdVideo = message.MediaId;
 			// let thumbMediaId = message.ThumbMediaId;
-			res.reply({
-				type: "text",
-				content:
-					"由于微信服务器原因，暂不支持视频 / video is not available yet due to wechat server"
-			});
+			// res.reply({
+			// 	type: "text",
+			// 	content:
+			// 		"由于微信服务器原因，暂不支持视频 / video is not available yet due to wechat server"
+			// });
+			let mediaIdVideo = message.MediaId;
+			// res.reply("video handling");
+			handleVideo(message, res);
 			break;
 		case "voice":
 			// ToUserName: 'gh_e96ffe097709',
